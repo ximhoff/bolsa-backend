2b07c0ea6af86d9213949de8ce972c1802c49143
ximhoff <ffohmi.epilef@gmail.com>
2020-05-13 19:03:14 -0300

    Criada funçao( e sua rota ) que retorna todos os usuarios

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,7 +3,11 @@ const Usuarios = use('App/Models/User');
 
 class UserController {
 
-  async autenticacao({ request, response, auth }) {
+  async index () {
+    return await Usuarios.all();
+  }
+
+  async autenticacao ({ request, response, auth }) {
     try {
       const { email, senha } = request.all();
       const usuarioAutorizado = await auth.attempt(email, senha);
@@ -19,7 +23,7 @@ class UserController {
     }
   }
   
-  async criar({ request, response }) {
+  async criar ({ request, response }) {
     try {
       const data = request.only(['nome', 'email', 'cpf', 'password', 'cargo', 'perfil_id', 'unidade_armazenadora_id']);
       const usuario = await Usuarios.create(data);
--- start/routes.js
+++ start/routes.js
@@ -1,26 +1,10 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
-
 Route.group(() => {
+  Route.get('/', 'UserController.index');
   Route.post('/login/', 'UserController.autenticacao');
   Route.post('/criar/', 'UserController.criar');
 }).prefix('usuarios');
